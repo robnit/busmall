d80fc41d34395cfda37eb2159c98f06ec1c956bc
Robin Miller <rmillerpdx@gmail.com>
2017-08-14 15:09:34 -0700

    add all image objects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Create blank array to store all image objects
 var imageArray = [];
 
 function ImageDisplay (name, filePath, elementId) {
@@ -20,24 +21,47 @@ function initializeImages(){
     var banana = new ImageDisplay('Banana', 'image/banana.jpg', 'banana');
     var bathroom = new ImageDisplay('Bathroom', 'image/bathroom.jpg', 'bathroom');
     var boots = new ImageDisplay('Boots', 'image/boots.jpg', 'boots');
+    var breakfast = new ImageDisplay('Breakfast','image/breakfast.jpg','breakfast');
+    var bubblegum = new ImageDisplay('Bubblegum','image/bubblegum.jpg','bubblegum');
+    var chair = new ImageDisplay('Chair','image/chair.jpg','chair');
+    var cthulhu = new ImageDisplay('Cthulhu','image/cthulhu.jpg','cthulhu');
+    var dogduck = new ImageDisplay('Dog-Duck','image/dog-duck.jpg','dogduck');
+    var dragon = new ImageDisplay('Dragon','image/dragon.jpg','dragon');
+    var pen = new ImageDisplay('Pen','image/pen.jpg','pen');
+    var petsweep = new ImageDisplay('Pet Sweep','image/pet-sweep.jpg','petsweep');
+    var scissors = new ImageDisplay('Scissors','image/scissors.jpg','scissors');
+    var shark = new ImageDisplay('Shark','image/shark.jpg','shark');
+    var sweep = new ImageDisplay('Sweep','image/sweep.jpg','sweep');
+    var tauntaun = new ImageDisplay('Tauntaun','image/tauntaun.jpg','tauntaun');
+    var unicorn = new ImageDisplay('Unicorn','image/unicorn.jpg','unicorn');
+    var usb = new ImageDisplay('USB','image/usb.jpg','usb');
+    var watercan = new ImageDisplay('Water Can','image/water-can.jpg','watercan');
+    var wineglass = new ImageDisplay('Wine Glass','image/wine-glass.jpg','wineglass');
 }
 
 initializeImages();
 
+//create containers for images
 var container1 = document.getElementById('image1');
 var container2 = document.getElementById('image2');
 var container3 = document.getElementById('image3');
 
+//create images
 var firstImage = document.createElement('img');
 var secondImage = document.createElement('img');
 var thirdImage = document.createElement('img');
 
+//Generate three random indices for image Array
+
+
+//Set src attribute to images
 firstImage.setAttribute( 'src', imageArray[0].filePath );
 secondImage.setAttribute( 'src', imageArray[1].filePath );
 thirdImage.setAttribute( 'src', imageArray[2].filePath );
 
-//Display Images
+//TO DO: Display image.name property alongside corresponding image
 
+//Display Images
 image1.appendChild(firstImage);
 image2.appendChild(secondImage);
 image3.appendChild(thirdImage);
