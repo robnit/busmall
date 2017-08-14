592001c5d527e7b1da871e5697aa32967e9124e3
Robin Miller <rmillerpdx@gmail.com>
2017-08-14 14:27:30 -0700

    create code to assign attribute to img element

--- js/app.js
+++ js/app.js
@@ -16,11 +16,13 @@ ImageDisplay.prototype.addToDom = function() {
 };
 
 function initializeImages(){
-    var bag = new ImageDisplay(Bag, 'image/bag.jpg', bag);
-    var banana = new ImageDisplay(Banana, 'image/banana.jpg', banana);
-    var bathroom = new ImageDisplay(Bathroom, 'image/bathroom', bathroom);
-    var boots = new ImageDisplay(Boots, 'image/boots.jpg', boots);
+    var bag = new ImageDisplay('Bag', 'image/bag.jpg', 'bag');
+    var banana = new ImageDisplay('Banana', 'image/banana.jpg', 'banana');
+    var bathroom = new ImageDisplay('Bathroom', 'image/bathroom', 'bathroom');
+    var boots = new ImageDisplay('Boots', 'image/boots.jpg', 'boots');
 }
+
+initializeImages();
 
 var container1 = document.getElementById('image1');
 var container2 = document.getElementById('image2');
@@ -29,3 +31,10 @@ var container3 = document.getElementById('image3');
 var firstImage = document.createElement('img');
 var secondImage = document.createElement('img');
 var thirdImage = document.createElement('img');
+
+firstImage.setAttribute( 'src', imageArray[0].filePath );
+secondImage.setAttribute( 'src', imageArray[1].filePath );
+thirdImage.setAttribute( 'src', imageArray[2].filePath );
+
+//Display Images
+
